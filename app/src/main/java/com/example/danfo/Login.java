a3a2d99ef89619d957489acbddc2f5a3268601b2
daveedson <daveeddrumz@gmail.com>
2020-01-16 08:40:27 +0100

    Danfo Layout and Intent

---
+++ app/src/main/java/com/example/danfo/Login.java
@@ -0,0 +1,38 @@
+package com.example.danfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Login extends AppCompatActivity{
+    private ImageView btnsplashlogin;
+    private ImageView btnsplashsignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnsplashlogin = findViewById(R.id.btnsplashlogin);
+        btnsplashsignup = findViewById(R.id.btnsplashsignup);
+
+
+
+
+        btnsplashlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SplashLogin.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/danfo/SplashLogin.java
@@ -0,0 +1,15 @@
+package com.example.danfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_login);
+
+    }
+}
